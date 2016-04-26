e455bb798dd82c73e80c097a3fbf55a54ac5bd2d
Kimiko Kano <kskano@gmail.com>
2016-04-26 11:40:32 -0600

    Trying to add serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ItemsController < ApiController
       def index
-        respond_with Item.all
+        respond_with Item.all, serializer: ItemSerializer
       end
     end
   end
